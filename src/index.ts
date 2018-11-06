6ec620025f07587a182a461838ea8e097492c18f
Jakob Pennington <jakobpennington@gmail.com>
2018-11-06 20:25:32 +1030

    Return errors in web responses

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 // Node imports
 import * as express from 'express';
-import * as helmet from 'helmet';
 import * as fs from 'fs';
+import * as helmet from 'helmet';
 import * as http from 'http';
 import * as https from 'https';
 
 // App imports
+import { appConfig } from './config/app';
 import routes from './routes';
-import { appConfig } from './config/app'; 
 
 // Bootstrap the express application
 const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 
@@ -28,14 +28,12 @@ const privateKey  = fs.readFileSync(appConfig.key, 'utf8');
 const certificate = fs.readFileSync(appConfig.certificate, 'utf8');
 const credentials = {key: privateKey, cert: certificate};
 
-console.log(`Loaded certificate from ${appConfig.certificate} and key from ${appConfig.key}`); 
-
-// Create and start the server 
+// Create and start the server
 const httpServer = http.createServer( app);
 const httpsServer = https.createServer(credentials, app);
 httpServer.listen(appConfig.portHttp);
 httpsServer.listen(appConfig.portHttps);
 
-
 // Log for visibiity
-console.log(`${appConfig.name} listening on HTTP port ${appConfig.portHttp} and HTTPS port ${appConfig.portHttps}`); 
+console.log(`[+] Loaded certificate from ${appConfig.certificate} and key from ${appConfig.key}`);
+console.log(`[+] ${appConfig.name} listening on HTTP port ${appConfig.portHttp} and HTTPS port ${appConfig.portHttps}`);
--- src/routes/controllers/search.ts
+++ src/routes/controllers/search.ts
@@ -14,19 +14,21 @@ export const getSearch = (req: Request, res: Response) => {
     const firstName: string = req.query.firstName;
 
     // Set up a connection
-    var connection = new mssql.ConnectionPool(mssqlConfig);
+    const connection = new mssql.ConnectionPool(mssqlConfig);
 
     // Connect and submit the query
     connection.connect().then((pool: mssql.ConnectionPool) => {
-        var req = new mssql.Request(pool);
-        req.query(`SELECT * from vuln.users WHERE firstName LIKE '${firstName}' AND username != 'admin'`).then(function (result: mssql.IResult<any>) {
-            res.send(result.recordsets)
-            connection.close();
-        }).catch(function (err) {
-            console.log(err);
-            connection.close();
-        });
-    }).catch(function (err) {
+        const sqlRequest = new mssql.Request(pool);
+        sqlRequest.query(`SELECT * from vuln.users WHERE firstName LIKE '${firstName}' AND username != 'admin'`)
+            .then((result: mssql.IResult<any>) => {
+                res.send(result.recordsets);
+                connection.close();
+            }).catch((err: Error) => {
+                console.log(err);
+                res.send(JSON.stringify(err));
+                connection.close();
+            });
+    }).catch((err: Error) => {
         console.log(err);
     });
 
